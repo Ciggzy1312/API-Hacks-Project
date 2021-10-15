248f425d4e639fc5d8710d320bc86a377abd4dc0
Deepayan <deepayanmukherjee1312@gmail.com>
2021-10-16 01:35:19 +0530

    Added post method of pin

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import { Room, Star, StarBorder } from "@material-ui/icons";
+import axios from 'axios'
 import './App.css';
 
 function App() {
@@ -18,6 +19,12 @@ function App() {
     long : null
   })
 
+  const [pins, setPins] = useState([])
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [currentUsername, setCurrentUsername] = useState('John')
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [rating, setRating] = useState(1);
   const [status, setStatus] = useState('')
   
   const getLocation = () => {
@@ -34,6 +41,51 @@ function App() {
       }, () => {
         setStatus('Unable to retrieve your location');
       });
+    }
+  }
+
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data)
+        console.log(res.data)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, [])
+
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    const newPin = {
+      username: currentUsername,
+      title,
+      desc,
+      rating,
+      lat: parseInt(location.lat),
+      long: parseInt(location.long),
+    }
+
+    try {
+      if(newPin.username && newPin.title && newPin.desc && newPin.lat && newPin.long){
+        const res = await axios.post('/pins', newPin)
+        setPins([...pins, res.data]);
+        setTitle('')
+        setDesc('')
+        setLocation({
+          lat : '',
+          long : ''
+        })
+      }
+      else{
+        alert('Please enter valid data...')
+      }
+    } catch (err) {
+      console.log(err)
     }
   }
 
@@ -56,12 +108,37 @@ function App() {
       </ReactMapGL>
 
       <div className='sidebar'>
-        <form className='sb-form'>
-          
+        <form className='sb-form' onSubmit={handleSubmit}>
+        <label>Title</label>
+            <input
+              value = {title}
+              placeholder="Enter a title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <label>Description</label>
+            <textarea
+              value = {desc}
+              placeholder="Say us something about this place."
+              onChange={(e) => setDesc(e.target.value)}
+            />
+            <label>Rating</label>
+            <select onChange={(e) => setRating(e.target.value)}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <label>Latitude</label>
+            <input value={location.lat} />
+
+            <label>Longitude</label>
+            <input value={location.long} />
+          <button type="submit" className="submitButton">Add Pin</button>
         </form>
         <button onClick={getLocation}>Get Location</button>
-        {location.lat && <p>Latitude: {location.lat}</p>}
-        {location.long && <p>Longitude: {location.long}</p>}
+        {/*{location.lat && <p>Latitude: {location.lat}</p>}
+        {location.long && <p>Longitude: {location.long}</p>}*/}
       </div>
     </div>
   );
